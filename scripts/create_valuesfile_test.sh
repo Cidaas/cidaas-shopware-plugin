ae4cfd185d78c15dc57d35429575e1e0f251c5da
Tujit Bora <tujit.bora@widas.in>
2023-05-16 19:05:52 +0530

    fix(): add shop domain

--- scripts/create_valuesfile_test.sh
+++ scripts/create_valuesfile_test.sh
@@ -8,12 +8,15 @@ image: $DOCKER_IMAGE
 port: 80
 runtimeId: wimcon1-dev-01
 tenantKey: cidaas-kube-nightlybuild-dev
+healthCheckPath: /
 cpu:
   min: 100m
   max: 500m
 memory:
   min: 128Mi
   max: 512Mi
+env:
+  SHOP_DOMAIN: kube-nightlybuild-dev.cidaas.de/shopware
 ingress:
   - hostname: kube-nightlybuild-dev.cidaas.de
     secretName: kube-nightlybuild-dev.cidaas.de
