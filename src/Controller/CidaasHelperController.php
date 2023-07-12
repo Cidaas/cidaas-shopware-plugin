2a44d2f170d0c4d480cf2a93f030a3099e608a4a
“anurag.verma” <anurag.verma@widas.in>
2023-07-12 13:47:14 +0530

    fix(): error handling in update profile

--- src/Controller/CidaasHelperController.php
+++ src/Controller/CidaasHelperController.php
@@ -370,31 +370,32 @@ use Cidaas\OauthConnect\Util\CidaasStruct;
         $salutationId = $request->get('salutationId');
         $res = $this->loginService->updateProfile($firstName, $lastName, $salutationId, $sub, $context);
         if($res) {
-              // Assuming $object is your stdClass object
-                $responseData = json_decode(json_encode($res), true);
-                // Now you can use $array with array_key_exists()
-                if (array_key_exists('success', $responseData) && $responseData['success'] === true) {
-                    // Key exists in the array
+             // Assuming $object is your stdClass object
+             $responseData = json_decode(json_encode($res), true);
+             // Now you can use $responseData with array_key_exists()
+             if (array_key_exists('success', $responseData)){
+                  // Key exists in the responseData
+                if( $responseData['success'] === true){
                     $this->addFlash('success', 'Successfully updated profile');
-                } elseif (array_key_exists('success', $responseData) && $responseData['success'] === false){
-                    if (array_key_exists('error', $responseData)) {
-                    // Handle error data
-                    // Extract error details
-                    $error = $responseData['error'];
-                    $errorMessage = $error['error'];
-                    $this->addFlash('danger', 'Failed to update profile: '.$errorMessage);
+                } elseif ($responseData['success'] === false) {
+                        if (array_key_exists('error', $responseData)) {
+                            // Handle error data
+                            // Extract error details
+                            $error = $responseData['error'];
+                            $errorMessage = $error['error'];
+                            $this->addFlash('danger', 'Failed to update profile: '.$errorMessage);
+                        } else {
+                            // No error information available
+                            error_log(json_encode($responseData));
+                            $this->addFlash('danger', 'Failed to update profile for unknown reason. Please check error log for more details.');
+                        }
                 } else {
-                    // No error information available
-                    error_log(json_encode($responseData));
-                    $this->addFlash('danger', 'Failed to update profile for unknown reason. Please check error log for more details.');
-                }
-                } else {
-                    // Key does not exist in the array
                     $this->addFlash('danger', 'Failed to update profile for unknown reason.');
                 }
-        } else {
-            $this->addFlash('danger', 'Failed to update profile for unknown reason.');
-        }
+             } else {
+                // Key does not exist in the array
+                $this->addFlash('danger', 'Failed to update profile for unknown reason.');
+            }
         return $this->redirectToRoute('frontend.account.profile.page');
     }
 
