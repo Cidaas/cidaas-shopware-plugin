788f3315830a959a081edded973fdf48afc4d20f
“anurag.verma” <anurag.verma@widas.in>
2023-07-28 13:21:02 +0530

    feat(): jquey removed

--- src/Resources/app/storefront/src/cidaas-login/cidaas-login.plugin.js
+++ src/Resources/app/storefront/src/cidaas-login/cidaas-login.plugin.js
@@ -1,77 +1,73 @@
-import Plugin from 'src/plugin-system/plugin.class'
+import Plugin from 'src/plugin-system/plugin.class';
 import DomAccess from 'src/helper/dom-access.helper';
 import HttpClient from 'src/service/http-client.service';
-import Debouncer from 'src/helper/debouncer.helper';
-import ElementLoadingIndicatorUtil from 'src/utility/loading-indicator/element-loading-indicator.util';
 import ButtonLoadingIndicatorUtil from 'src/utility/loading-indicator/button-loading-indicator.util';
-import * as $ from 'jquery'
 
 export default class CidaasLogin extends Plugin {
     static options = {
-
-    }
+        // Add your options here if needed
+    };
     
     init() {
-        this.client = new HttpClient()
-        this.email = null
-        this.password = null
-        this.nextButton = DomAccess.querySelector(document, '#nextButton')
-        this.nextLoadingThingy = new ButtonLoadingIndicatorUtil(this.nextButton)
-        $('#nextButton').on('click', (evt) => {
+        this.client = new HttpClient();
+        this.email = null;
+        this.password = null;
+        this.nextButton = DomAccess.querySelector(document, '#nextButton');
+        this.nextLoadingThingy = new ButtonLoadingIndicatorUtil(this.nextButton);
+        this.nextButton.addEventListener('click', (evt) => {
             if (this.email === null) {
-                this.email = $('#email').val()
-                this.nextLoadingThingy.create()
-                this.client.post(this.options.cidaas+'/users-srv/user/checkexists/'+this.options.requestId, JSON.stringify({
+                this.email = document.querySelector('#email').value;
+                this.nextLoadingThingy.create();
+                this.client.post(this.options.cidaas + '/users-srv/user/checkexists/' + this.options.requestId, JSON.stringify({
                     email: this.email,
                     requestId: this.options.requestId
                 }), (res) => {
                     if (res) {
-                        let result = JSON.parse(res)
+                        let result = JSON.parse(res);
                         if (result.success) {
                             this.client.post('/cidaas/exists', JSON.stringify({
                                 email: this.email
                             }), (res2) => {
-                                let result2 = JSON.parse(res2)
+                                let result2 = JSON.parse(res2);
                                 if (result2.exists) {
-                                    this.client.get('/cidaas/lastlogin/'+result2.id, (res3) => {
-                                        let result3 = JSON.parse(res3)
+                                    this.client.get('/cidaas/lastlogin/' + result2.id, (res3) => {
+                                        let result3 = JSON.parse(res3);
                                         if (result3.lastLogin !== null) {
-                                            $('#emailContainer').hide()
-                                            $('#passwordContainer').show()
-                                            $('#nextButton').text('Login')
-                                            this.nextLoadingThingy.remove()
-                                            $('#password').focus()
+                                            document.querySelector('#emailContainer').style.display = 'none';
+                                            document.querySelector('#passwordContainer').style.display = 'block';
+                                            document.querySelector('#nextButton').textContent = 'Login';
+                                            this.nextLoadingThingy.remove();
+                                            document.querySelector('#password').focus();
                                         } else {
-                                            $('#emailContainer').hide()
-                                            $('#onetimePassword').show()
-                                            this.nextLoadingThingy.remove()
-                                            $('#nextButton').hide();
-                                            $('#toOtpLink').attr('href', this.options.cidaas+'/identity/login/initiate?userIdHint='+encodeURI(this.email)+'&requestId='+this.options.requestId+'&type=email')
+                                            document.querySelector('#emailContainer').style.display = 'none';
+                                            document.querySelector('#onetimePassword').style.display = 'block';
+                                            this.nextLoadingThingy.remove();
+                                            document.querySelector('#nextButton').style.display = 'none';
+                                            document.querySelector('#toOtpLink').setAttribute('href', this.options.cidaas + '/identity/login/initiate?userIdHint=' + encodeURI(this.email) + '&requestId=' + this.options.requestId + '&type=email');
                                         }
-                                    })
+                                    });
                                 }
-                            })
+                            });
                         } else {
-                            $('#emailContainer').hide()
-                            $('#nextButton').hide()
-                            this.nextLoadingThingy.remove()
-                            $('#complete').show()
+                            document.querySelector('#emailContainer').style.display = 'none';
+                            document.querySelector('#nextButton').style.display = 'none';
+                            this.nextLoadingThingy.remove();
+                            document.querySelector('#complete').style.display = 'block';
                         }
                     } else {
-                        window.location.href='/cidaas/register?userIdHint='+this.email+'&type=email'
+                        window.location.href = '/cidaas/register?userIdHint=' + this.email + '&type=email';
                     }
                     
-                })
+                });
             } else if (this.password === null) {
-                this.password = $('#password').val()
-                $('#loginForm').attr('action', 'https://my-test.mainz05.de/login-srv/login')
-                $('#loginForm').trigger('submit')
+                this.password = document.querySelector('#password').value;
+                document.querySelector('#loginForm').setAttribute('action', 'https://my-test.mainz05.de/login-srv/login');
+                document.querySelector('#loginForm').dispatchEvent(new Event('submit'));
             }
-        })
+        });
     }
-// ElementLoadingIndicatorUtil.create(this.mediaDiv)
+    
     handleFormResponse(response) {
-        // window.location.reload()
-
+        // window.location.reload();
     }
 }
