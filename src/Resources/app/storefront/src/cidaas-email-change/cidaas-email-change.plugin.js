7e7e2c1fa4078f0bf6b3697ff05cceab24c4c3c5
Tujit Bora <tujit.bora@widas.in>
2023-07-28 19:19:46 +0530

    fix(): email change submit issue fix

--- src/Resources/app/storefront/src/cidaas-email-change/cidaas-email-change.plugin.js
+++ src/Resources/app/storefront/src/cidaas-email-change/cidaas-email-change.plugin.js
@@ -1,38 +1,31 @@
-import Plugin from 'src/plugin-system/plugin.class';
+import Plugin from 'src/plugin-system/plugin.class'
 import DomAccess from 'src/helper/dom-access.helper';
 import HttpClient from 'src/service/http-client.service';
-import Debouncer from 'src/helper/debouncer.helper';
 import ElementLoadingIndicatorUtil from 'src/utility/loading-indicator/element-loading-indicator.util';
-import ButtonLoadingIndicatorUtil from 'src/utility/loading-indicator/button-loading-indicator.util';
-import CidaasUtil from '../util/cidaas-util';
 
 export default class CidaasEmailChange extends Plugin {
-
     init() {
         this.client = new HttpClient();
         const emailForm = document.getElementById('emailForm');
-        console.log(emailForm)
         emailForm.addEventListener('submit', this.handleSubmit.bind(this));
-        // ElementLoadingIndicatorUtil.create(this.mediaDiv);
         this.mailContainer = DomAccess.querySelector(document, 'div#accountMailContainer');
     }
 
     sleep(ms) {
         return new Promise(resolve => {
-            setTimeout(resolve, ms);
-        });
+            setTimeout(resolve, ms)
+        })
     }
 
     handleSubmit(evt) {
+
         evt.preventDefault();
 
-        console.log(evt)
         let email1 = document.getElementById('personalMail').value;
         let email2 = document.getElementById('personalMailConfirmation').value;
         if (email1 === email2) {
             document.getElementById('personalMailConfirmation').classList.remove('is-invalid');
             document.getElementById('invalidFeedback').style.display = 'none';
-
             this.changeEmail(email1, email2);
         } else {
             document.getElementById('invalidFeedback').style.display = 'block';
@@ -41,7 +34,7 @@ export default class CidaasEmailChange extends Plugin {
     }
 
     async changeEmail(email1, email2) {
-        this.email = email1;
+        this.email = email1
         document.getElementById('emailForm').style.display = 'none';
         document.getElementById('emailVerifySpan').textContent = email1;
         document.getElementById('verifyThing').style.display = 'block';
@@ -49,17 +42,13 @@ export default class CidaasEmailChange extends Plugin {
     }
 
     handleVerify() {
-        ElementLoadingIndicatorUtil.create(this.mailContainer);
-        this.client.post(
-            '/cidaas/emailform',
-            JSON.stringify({
-                email: this.email
-            }),
-            (res) => {
-                ElementLoadingIndicatorUtil.remove(this.mailContainer);
-                document.getElementById('verifyThing').style.display = 'none';
-                window.location.href = '/account';
-            }
-        );
+        ElementLoadingIndicatorUtil.create(this.mailContainer)
+        this.client.post('/cidaas/emailform', JSON.stringify({
+            email: this.email
+        }), (res) => {
+            ElementLoadingIndicatorUtil.remove(this.mailContainer)
+            document.getElementById('verifyThing').style.display = 'none'
+            window.location.href = "/account"
+        })
     }
 }
