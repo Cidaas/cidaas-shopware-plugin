da8a8eec840eb08b7cdfacb2e543933b0586204c
“anurag.verma” <anurag.verma@widas.in>
2023-10-27 14:07:21 +0530

    feat(): get customer group changes

--- src/Subscriber/CustomerGroupSubscriber.php
+++ src/Subscriber/CustomerGroupSubscriber.php
@@ -4,9 +4,21 @@ namespace Cidaas\OauthConnect\Subscriber;
 
 use Shopware\Core\Framework\DataAbstractionLayer\Event\EntityWrittenEvent;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Shopware\Core\Framework\DataAbstractionLayer\EntityRepository;
+use Shopware\Core\Framework\DataAbstractionLayer\Search\Criteria;
+use Shopware\Core\Framework\Context;
+use Shopware\Core\Framework\DataAbstractionLayer\Search\Filter\EqualsFilter;
 
 class CustomerGroupSubscriber implements EventSubscriberInterface
 {
+    private $customerGroupTranslationRepo;
+
+    public function __construct(
+        EntityRepository $customerGroupTranslationRepo
+    ) {
+        $this->customerGroupTranslationRepo = $customerGroupTranslationRepo;
+    }
+
     public static function getSubscribedEvents(): array
     {
         return [
@@ -16,15 +28,29 @@ class CustomerGroupSubscriber implements EventSubscriberInterface
 
     public function onCustomerGroupWritten(EntityWrittenEvent $event)
     {
-        // Check if this event corresponds to a customer group creation or update
-        foreach ($event->getWriteResults() as $writeResult) {
-            if ($writeResult->getEntityName() === 'customer_group') {
-                $customerGroup = $writeResult->getEntity();
+         $writtenEntities = $event->getWriteResults();
 
-                // Your code to create or modify the customer group here
-                // Example: $customerGroup->setName('Your New Group Name');
-                // /Resources/app/administration/src/module/sw-settings-customer-group
-            }
-        }
+         foreach ($writtenEntities as $entity) {
+             // Check if the entity is a customer group
+             if ($entity->getEntityName() === 'customer_group') {
+                 // Get the customer group data
+                 $customerGroupData = $entity->getPayload(); 
+             }
+         }
+
+         $criteria = new Criteria();
+         $criteria->addFilter(new EqualsFilter('id', $id));
+         $customerGroupName = $this->customerGroupTranslationRepo->search($criteria, Shopware\Context::createDefaultContext())->first();
+         error_log($customerGroup->getTranslation('name'));
+         
+        //  $this->createUrls('aa9b4ad2128946abb182a698b1056ca5', $event->getContext());
+    }
+
+    private function createUrls(string $id, Context $context): void
+    {
+        $criteria = new Criteria();
+        $criteria->addFilter(new EqualsFilter('id', $id));
+        $customerGroupName = $this->customerGroupTranslationRepo->search($criteria, Shopware\Context::createDefaultContext())->first();
+        error_log($customerGroup->getTranslation('name'));
     }
 }
